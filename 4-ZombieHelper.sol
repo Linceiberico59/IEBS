9f5118055c6b701cb7a8b1f2ed4b96d58f81318d
Francisco José Medrano <66084146+Linceiberico59@users.noreply.github.com>
2020-10-23 18:08:36 +0200

    Add files via upload

---
+++ 4-ZombieHelper.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.19;
+
+import "./ZombieFeeding.sol";
+
+contract ZombieHelper is ZombieFeeding { 
+    
+    modifier aboveLevel(uint _level, uint _zombieId) {
+        //Comprobación de que zombies[_zombieId].level >= _level
+        require(zombies[_zombieId].level >= _level);
+        _;
+    }
+
+    function changeName(uint _zombieId,  string _newName) external aboveLevel (2, _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
+    }
+
+    function changeDna(uint _zombieId, uint _newDna) external aboveLevel (20, _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
+    } 
+
+    function getZombiesByOwner(address _owner) external view returns(uint[]) {
+        uint[] memory result = new uint[](ownerZombieCount[_owner]);
+        return result;
+
+    }
+ }
