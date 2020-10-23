b2102278d0da0f362a1bd30d9cbf101de2ecafbc
Francisco José Medrano <66084146+Linceiberico59@users.noreply.github.com>
2020-10-23 10:51:33 +0200

    Update 9-ZombieFeeding.sol

--- 9-ZombieFeeding.sol
+++ 9-ZombieFeeding.sol
@@ -30,7 +30,7 @@ contract ZombieFeeding is ZombieFactory {
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) /2;
     _createZombie("NoName", newDna);
-    if (keccak256(abi.encodePacked(_species)) == keccak256("kitty")) {
+    if (keccak256(_species) == keccak256("kitty")) {
       newDna = newDna - newDna % 100 + 99;
     }
 } 
